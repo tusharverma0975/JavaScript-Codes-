e8b699b649b83b8bd24fdd4780fd86d6992beb4d
Tushar Verma <117035856+tusharverma0975@users.noreply.github.com>
2024-09-23 22:38:14 +0530

    Add files via upload

---
+++ event_capturing_event_deligation/event.js
@@ -0,0 +1,12 @@
+document.querySelector("#grandParent").addEventListener('click',()=>{
+    console.log('grandparent cilck');
+    
+},false);
+document.querySelector("#parent").addEventListener('click',()=>{
+    console.log('parent cilck');
+    
+});
+document.querySelector("#child").addEventListener('click',()=>{
+    console.log('child cilck');
+    
+}); 
